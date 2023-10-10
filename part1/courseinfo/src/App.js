cff87dd6784a8080444321092f96952cb6ca0c0b
Lien <hwenyi@outlook.my>
2023-10-10 18:58:05 +0800

    完成练习1.1和1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+}
+
+const Content = (props) => {
+  return (
+    <Part name={props.name} exercises={props.exercises} />
+  );
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises}</p>
+    </div>
+  );
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      {course.parts.map((part, index) => (
+        <Content
+          key={index}
+          name={part.name}
+          exercises={part.exercises}
+        />
+      ))}
+      <Total exercises={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
     </div>
   );
 }
 
 export default App;
+
+
+
+
