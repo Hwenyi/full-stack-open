19b69905125b36515d9e27f3b110652ada4100f5
Lien <hwenyi@outlook.my>
2023-10-16 12:34:27 +0800

    添加build

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("build"));
 
 morgan.token("jsonBody", (req) => {
   return JSON.stringify(req.body);
