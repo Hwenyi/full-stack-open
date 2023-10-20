1deab6284b4d868b4ab4292854c420f652376328
Lien <hwenyi@outlook.my>
2023-10-20 21:55:52 +0800

    重做bloglist前端

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,62 +1,10 @@
-import './App.css'
-// eslint-disable-next-line no-unused-vars
-import { useState, useEffect, useRef } from 'react'
-import blogService from './services/blogs'
-import loginService from './services/login'
-import LoginForm from './components/LoginForm'
-import Togglable from './components/Togglable'
 
-const App = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
-
-  // const loginFormRef = useRef()
-
-  useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs))
-  }, [])
-
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    try {
-      const user = await loginService.login({ username, password })
-      setUser(user)
-      setUsername('')
-      setPassword('')
-      // blogService.setToken(user.token)
-      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      console.log('Wrong credentials')
-    }
-  }
-
-  // const handleLogout = () => {
-  //   window.localStorage.removeItem('loggedBlogAppUser')
-  //   setUser(null)
-  // }
-
+function App() {
   return (
     <div>
-      <h1>Notes app</h1>
-      {!user &&
-        <Togglable buttonLabel="log">
-          <LoginForm
-            username={username}
-            password={password}
-            handleUsernameChange={({ target }) => setUsername(target.value)}
-            handlePasswordChange={({ target }) => setPassword(target.value)}
-            handleSubmit={handleLogin}
-          />
-        </Togglable>
-      }
+      hello world
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
