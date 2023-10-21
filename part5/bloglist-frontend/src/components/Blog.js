abdb7e5129f313a2110b0148ab8611a17eb1bd53
Lien <hwenyi@outlook.my>
2023-10-21 16:51:34 +0800

    调整blog样式

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,42 +1,46 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const Blog = ({blog, updateLikes, deleteBlog, username}) => {
-  const [visible,setVisible] = useState(false)
+const Blog = ({ blog, updateLikes, deleteBlog, username }) => {
+  const [visible, setVisible] = useState(false);
 
   const toggleVisibility = () => {
-    setVisible(!visible)
-  }
+    setVisible(!visible);
+  };
 
-  const handleLike=() =>{
+  const handleLike = () => {
     const blogToUpdate = {
       title: blog.title,
       author: blog.author,
       url: blog.url,
       likes: blog.likes + 1,
-      uesr: blog.user.id
-    }
-    updateLikes(blog.id,blogToUpdate)
-  }
+      user: blog.user.id,
+    };
+    updateLikes(blog.id, blogToUpdate);
+  };
 
   const handleDelete = () => {
-    if(window.confirm(`Remove blog ${blog.title} by ${blog.author}`)){
-      deleteBlog(blog.id)
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
+      deleteBlog(blog.id);
     }
-  }
-  
+  };
+
   return (
-    <div className='blog'>
+    <div className="blog">
       <div>
-        <span className="title">{blog.title}</span>
-        <span classNmae="author">{blog.author}</span>
-        <button id='view-btn' onClick={toggleVisibility}>{visible ? 'hide' : 'show'}</button>
+        <span className="title">{blog.title} - </span>
+        <span className="author">{blog.author}</span>{" "}
+        <button id="view-btn" onClick={toggleVisibility}>
+          {visible ? "hide" : "show"}
+        </button>
       </div>
       {visible && (
         <div className="blog-details">
           <div>{blog.url}</div>
           <div>
-            Likes:{ blog.likes }{' '}
-            <button id="like-btn" onClick={handleLike}>Like</button>{' '}
+            Likes: {blog.likes}{" "}
+            <button id="like-btn" onClick={handleLike}>
+              like
+            </button>{" "}
           </div>
           <div>{blog.user.name}</div>
           {blog.user.username === username && (
@@ -47,7 +51,7 @@ const Blog = ({blog, updateLikes, deleteBlog, username}) => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
